e2f145c11ce6f513fe4256291887cb1e40a8324a
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-03-28 11:44:49 -0300

    Initial commit

---
+++ Assets/G1_Ej5.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G1_Ej5 : MonoBehaviour
+{
+    public int num1 = 0;
+    public int num2 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       if (num2 == 0) {
+       Debug.Log("No se puede dividir por cero");
+}
+       else {
+       Debug.Log(num1 + "/" + num2 + "=" + num1 / num2);
+}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/G1_Ej6.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G1_Ej6 : MonoBehaviour
+{
+    public int num1 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+      if (num1 <= 0) {
+      Debug.Log("No es válido, ingrese números mayores a 0");
+}
+      else if (num1 % 2 == 0) {
+      Debug.Log("El número es par");
+}
+      else {
+      Debug.Log("El número es impar");
+}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
